bfa5b29f6a8f11c145021f50d0fbe06dc4345851
Ledio <ledio.papa@fti.edu.al>
2019-05-08 02:13:55 +0300

    Implemented Map based Services. closes #6

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/map/PetServiceMap.java
@@ -0,0 +1,37 @@
+package guru.springframework.sfgpetclinic.service.map;
+
+import guru.springframework.sfgpetclinic.model.Pet;
+import guru.springframework.sfgpetclinic.service.CRUDService;
+
+import java.util.Set;
+
+/**
+ * Created by ledio on 5/8/19
+ */
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CRUDService<Pet, Long> {
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet pet) {
+        return super.save(pet.getId(), pet);
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Pet pet) {
+        super.delete(pet);
+    }
+}
---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/map/VetServiceMap.java
@@ -0,0 +1,37 @@
+package guru.springframework.sfgpetclinic.service.map;
+
+import guru.springframework.sfgpetclinic.model.Vet;
+import guru.springframework.sfgpetclinic.service.CRUDService;
+
+import java.util.Set;
+
+/**
+ * Created by ledio on 5/8/19
+ */
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CRUDService<Vet, Long> {
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Vet save(Vet vet) {
+        return super.save(vet.getId(), vet);
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Vet vet) {
+        super.delete(vet);
+    }
+}
