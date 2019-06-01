099e47133ea6813ae71250470d082470c5992139
Ledio <ledio.papa@fti.edu.al>
2019-06-01 22:47:55 +0300

    Added Owner index page and Controller. closes #13.

---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controller/OwnerController.java
@@ -0,0 +1,17 @@
+package guru.springframework.sfgpetclinic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Ledio Papa on 6/1/19
+ */
+@Controller
+public class OwnerController {
+
+    @RequestMapping({"/owners", "/owners/index", "/owners/index.html"})
+    public String listOwners() {
+        return "owners/index.html";
+    }
+
+}
