0e384f45af2ba08f8c61f0bd7ab8013e15dd1eb0
Ledio <ledio.papa@fti.edu.al>
2019-06-01 22:56:39 +0300

    Added Default Request Mapping path to "/owners" for controller of Owners

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controller/OwnerController.java
@@ -6,10 +6,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 /**
  * Created by Ledio Papa on 6/1/19
  */
+@RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
-    @RequestMapping({"/owners", "/owners/index", "/owners/index.html"})
+    @RequestMapping({"", "/index", "/index.html"})
     public String listOwners() {
         return "owners/index.html";
     }
