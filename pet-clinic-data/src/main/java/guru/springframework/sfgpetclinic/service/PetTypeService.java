2905505360890b50e23a7ff9b32ace3f13789da8
Ledio <ledio.papa@fti.edu.al>
2019-07-16 00:16:05 +0300

    added service impl. closes #29

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/PetTypeService.java
@@ -0,0 +1,9 @@
+package guru.springframework.sfgpetclinic.service;
+
+import guru.springframework.sfgpetclinic.model.PetType;
+
+/**
+ * Created by Ledio Papa on 7/16/19
+ */
+public interface PetTypeService extends CRUDService<PetType, Long> {
+}
---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/map/PetTypeServiceMap.java
@@ -0,0 +1,39 @@
+package guru.springframework.sfgpetclinic.service.map;
+
+import guru.springframework.sfgpetclinic.model.PetType;
+import guru.springframework.sfgpetclinic.service.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+/**
+ * Created by Ledio Papa on 7/16/19
+ */
+@Service
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+}
