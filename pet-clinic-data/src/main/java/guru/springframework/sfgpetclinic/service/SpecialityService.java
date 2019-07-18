67e9e6af30a66f2430ab823f34609b6c443a5a6e
Ledio <ledio.papa@fti.edu.al>
2019-07-19 00:07:00 +0300

    added specialities service map impl. closes #31

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/SpecialityService.java
@@ -0,0 +1,9 @@
+package guru.springframework.sfgpetclinic.service;
+
+import guru.springframework.sfgpetclinic.model.Speciality;
+
+/**
+ * Created by Ledio Papa on 7/18/19
+ */
+public interface SpecialityService extends CRUDService<Speciality, Long> {
+}
---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/map/SpecialityServiceMap.java
@@ -0,0 +1,39 @@
+package guru.springframework.sfgpetclinic.service.map;
+
+import guru.springframework.sfgpetclinic.model.Speciality;
+import guru.springframework.sfgpetclinic.service.SpecialityService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+/**
+ * Created by Ledio Papa on 7/19/19
+ */
+@Service
+public class SpecialityServiceMap extends AbstractMapService<Speciality, Long> implements SpecialityService {
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+}
