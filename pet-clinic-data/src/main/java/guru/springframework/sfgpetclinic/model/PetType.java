f5c8e8b2b3d4a4ae8284821af05713d4dc6d9874
ledio.papa@fti.edu.al <n25I^kW4bfhW>
2019-08-27 15:29:37 +0200

    added Lombok and refactored the JPA Entities closes #51

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -1,5 +1,7 @@
 package guru.springframework.sfgpetclinic.model;
 
+import lombok.*;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
@@ -7,18 +9,15 @@ import javax.persistence.Table;
 /**
  * Created by ledio on 5/7/19
  */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "type")
 public class PetType extends BaseEntity {
 
     @Column(name = "name")
     private String name;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
