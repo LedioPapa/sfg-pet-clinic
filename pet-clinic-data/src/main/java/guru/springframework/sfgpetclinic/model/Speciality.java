698f5a7b72fd2d9ee5a077ddbc3ba1784781b75c
ledio.papa@fti.edu.al <n25I^kW4bfhW>
2019-08-27 10:54:54 +0200

    updated Vet and Speciality with JPA entity mappings. closes #37.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Speciality.java
@@ -1,10 +1,17 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * Created by Ledio Papa on 7/15/19
  */
+@Entity
+@Table(name = "specialities")
 public class Speciality extends BaseEntity {
 
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
